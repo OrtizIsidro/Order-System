f71214ec9a85438e3dd0613341b76369ba57cddd
isidro <ortiz.isidro.159@gmail.com>
2022-07-24 07:12:15 -0300

    improve pizza's descriptions

--- android-app/src/components/flavors_menu/index.js
+++ android-app/src/components/flavors_menu/index.js
@@ -1,5 +1,5 @@
 import { Button } from "@react-native-material/core";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, Image } from "react-native";
 
 const FlavorsMenu = ({ data, addToCart }) => {
   return (
@@ -10,7 +10,9 @@ const FlavorsMenu = ({ data, addToCart }) => {
         <View style={{ flex: 1, margin: 10 }}>
           <Text>{item.title}</Text>
           <Text>{item.description}</Text>
-          <Text>{item.img}</Text>
+          <View>
+            <Image source={{ uri: item.img, height: 100, width: 100 }} />
+          </View>
           <Text>botton de sumar al carrito, imaginatelo por ahora plis</Text>
           <Text>{item.precio}</Text>
           <Text>{item.puntaje}</Text>
